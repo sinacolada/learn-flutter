506a95b20254a26b12283f4f54115cc064681bb7
Sina Soltanieh <sinasolt@Sinas-MacBook-Air.local>
2023-08-19 16:53:26 -0400

    project set up and home views

---
+++ android/app/src/main/kotlin/com/exitree/mynotes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.exitree.mynotes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:mynotes/views/login_view.dart';
+import 'package:mynotes/views/register_view.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const LoginView(),
+    ),
+  );
+}
