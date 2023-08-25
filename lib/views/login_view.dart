124f336f871f6e3c398b996f2ae4b04b75506ce1
Sina Soltanieh <sinasolt@Sinas-MacBook-Air.local>
2023-08-24 20:24:36 -0400

    error dialog and exhaustive error handling

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/constants/routes.dart' as routes;
-import 'dart:developer' as devtools show log;
+
+import '../utilities/show_error_dialog.dart';
+// import 'dart:developer' as devtools show log;
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -69,10 +71,26 @@ class _LoginViewState extends State<LoginView> {
                 }
               } on FirebaseAuthException catch (e) {
                 if (e.code == "user-not-found") {
-                  devtools.log("User not found");
+                  await showErrorDialog(
+                    context,
+                    "User not found",
+                  );
                 } else if (e.code == "wrong-password") {
-                  devtools.log("Wrong password");
+                  await showErrorDialog(
+                    context,
+                    "Wrong password",
+                  );
+                } else {
+                  await showErrorDialog(
+                    context,
+                    "Error ${e.code}",
+                  );
                 }
+              } catch (e) {
+                await showErrorDialog(
+                  context,
+                  e.toString(),
+                );
               }
             },
             child: const Text("Login"),
